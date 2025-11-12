9832f9835264118f82d0e81b69998493ac0a294d
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-11-12 20:33:30 +0400

    finished s3 v2 configuration

--- s3Service.js
+++ s3Service.js
@@ -1,13 +1,16 @@
 const { S3 } = require('aws-sdk');
 const uuid = require('uuid').v4;
 
-exports.s3UploadV2 = async () => {
+exports.s3UploadV2 = async (file) => {
   const s3 = new S3();
 
   const param = {
     Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: `${uuid()}-sample.txt`,
+    Key: `uploads/${uuid()}-${file.originalname}`,
+    Body: file.buffer,
   };
 
-  s3.upload
+  const result = await s3.upload(param).promise();
+  
+  return result;
 };
